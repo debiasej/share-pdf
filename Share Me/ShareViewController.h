73b2c3ce1b5c5cd76d0310ded485e11e16b2ea6e
Mario de Biase <debiasej@gmail.com>
2017-01-26 16:34:53 +0100

    Add Share Extension

---
+++ Share Me/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Share Me
+//
+//  Created by Mario de Biase on 26/1/17.
+//  Copyright © 2017 debiasej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
