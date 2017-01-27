661052695112849352519b8a1b91294383583af3
Mario de Biase <debiasej@gmail.com>
2017-01-27 11:45:49 +0100

    Refactor: directory restructuring

---
+++ sharePDF/Model/pdfDocument.h
@@ -0,0 +1,13 @@
+//
+//  pdfDocument.h
+//  sharePDF
+//
+//  Created by Mario de Biase on 27/1/17.
+//  Copyright © 2017 debiasej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pdfDocument : UIDocument
+
+@end
---
+++ sharePDF/Model/pdfDocument.m
@@ -0,0 +1,13 @@
+//
+//  pdfDocument.m
+//  sharePDF
+//
+//  Created by Mario de Biase on 27/1/17.
+//  Copyright © 2017 debiasej. All rights reserved.
+//
+
+#import "pdfDocument.h"
+
+@implementation pdfDocument
+
+@end
