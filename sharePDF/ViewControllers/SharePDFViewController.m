bfb2b7011e4c266ba907074152059134c003489a
Mario de Biase <debiasej@gmail.com>
2017-01-30 13:24:22 +0100

    Retrieve documents from shared app group sandbox and show its titles

--- sharePDF/ViewControllers/SharePDFViewController.m
+++ sharePDF/ViewControllers/SharePDFViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SharePDFViewController.h"
+#import "NSFileManager+FileUrlForDocumentNamed.h"
 
 static NSString *const appGroupId = @"group.debiasej.sharePDF";
 static NSString *const dictKey = @"dictPdf";
@@ -27,22 +28,28 @@ static NSString *const dictKey = @"dictPdf";
     self.pdfList =  [NSMutableArray new];
 }
 
--(void)cofigureTableview
-{
-    [self.view addSubview:self.tableview];
-}
-
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:YES];
+    
+    //[self readPDFsFromUserDefaults];
+    [self readPDFsFromFileSystem];
+}
+
+- (void)readPDFsFromUserDefaults {
     NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:appGroupId];
     NSDictionary *dict = [defaults valueForKey:dictKey];
     
     if (dict) {
-        NSData *pdfData = [dict valueForKey:@"pdfData"];
+        //NSData *pdfData = [dict valueForKey:@"pdfData"];
         NSString *fileName = [dict valueForKey:@"name"];
         
         [self.pdfList addObject:fileName];
     }
+}
+
+- (void) readPDFsFromFileSystem {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    self.pdfList = [[fileManager getAllPDFsInFileSystem] mutableCopy];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -56,15 +63,19 @@ static NSString *const dictKey = @"dictPdf";
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"myCell";
-    
     UITableViewCell *cell = [self.tableview dequeueReusableCellWithIdentifier:cellIdentifier];
     
     if(cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
         
     }
-    cell.textLabel.text =  [self.pdfList objectAtIndex:indexPath.row];
+    cell.textLabel.text =  [self documentTitleAt:indexPath.row];
     return cell;
+}
+
+- (NSString *) documentTitleAt:(NSUInteger) index {
+    PDFDocument *pdf = (PDFDocument *)[self.pdfList objectAtIndex:index];
+    return pdf.title;
 }
 
 @end
