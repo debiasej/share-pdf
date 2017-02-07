e3afc29cc93d581bbe7de3bb73e7b7a6c9faf2d3
Mario de Biase <debiasej@gmail.com>
2017-02-07 13:03:21 +0100

    Create a jQuery component to list all PDF documents

---
+++ server/src_jquery/app.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+    $(document).on('change', ':checkbox', function() {
+        disableOthers.call(this);
+        pdfDidSelected();
+    });
+
+    function disableOthers() {
+        var $othersCheckboxes = $('#documentList').find('input:checkbox').not($(this));
+
+        if ($(this).prop('checked')) {
+            $othersCheckboxes.attr('disabled', true);
+        } else {
+            $othersCheckboxes.attr('disabled', false);
+        }
+    }
+
+    function pdfDidSelected() {
+        window.webkit.messageHandlers.SharePDFObserver.postMessage($('input:checked').val());
+    }
+
+});
+
+function loadPDFList(fileNamesArray) {
+    fileNamesArray.forEach(function(element, index) {
+
+        const checkbox = `<input class="doc" id="doc-${index}" type="checkbox" value="${element}"> ${element}`;
+        const checkContainer = `<div class="checkbox-container">${checkbox}</div>`;
+
+        $('#documentList').append(checkContainer);
+    });
+}
+
+
+function API() {
+
+}
